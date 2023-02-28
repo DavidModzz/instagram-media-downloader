50bd094a768d40d73e753acd59b48029940a55e0
Arom <66982638+Aromakelapa@users.noreply.github.com>
2023-02-28 00:55:57 +0000

    Fix

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 const igdown = require('./index.js');
 
-igdl('https://www.instagram.com/reel/CpG_5lqDJsh/')
+igdown('https://www.instagram.com/reel/CpG_5lqDJsh/')
   .then((res) => {
     console.log(res);
   })
